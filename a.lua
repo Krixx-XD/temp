19ad468037d08f8daf8ad1562ba6ff6111cb03e7
Krixx <161109688+Krixx-XD@users.noreply.github.com>
2024-11-22 14:46:56 +0500

    Create a.lua

---
+++ a.lua
@@ -0,0 +1,105 @@
+[..3[.[.;.[;>{>{:$#>:{R:>{$#>:#RR>:RRRRR#R${>:RRRR{R>:RRRrr3r.4">#$?>{>>{{>{?.[{?D{?[#}{.3/]e?{}:#E>D?{:3/d./d3E./[]#ED]3D}>.]d3}>#/]3e/.d]d]#"?DE/]'E#]'/?#D'?D}{>#E>P#e>P#>.{PD{>P}d{>P}#ED[.p]ed#[.p]ed#][e
+            '/ED'/e#D/'#ED
+            ?"E
+            e3/'
+            e3?"e3d
+            ?"#UMED
+            34m
+            435
+            345
+
+            5
+            345
+            34
+            53
+            45
+            345
+      y999y 9 yy9 9 y9 y y9 y9 y7 y7 y9y97 y9y9 9y79yy9y9 9y7997 y y9 y99 y y9 y9 y9 y7y7y7y7  y7 y7 3ey7dy30 y0 y0 4y0y7d00 y9 y9dy09
+            $#)( )(# )(R )($#)( R#$(R )(*R$()*)(*$#(R )*30 89R#R*)( 3 )R 0984 )(*$ )(4#R 0984r 4*)(R4 89)4 8094#R *)(4#0 9843 0984# 0948#049*#049r#*409r#*
+          if0# J)#*D UU) )0u u u 0u u0 0u0u)U )U U)U) UU U  U U U U) U) U) )U**U )#$ U) U)%U)%)*)U)#% U)
+( #$ (#$(( $( *#$( *#$(( #( * ($*#( *9 898 -99-- 9- 98-8-8-8-83RF
+#F
+#FR
+#F#
+rF
+) #  $$*&( #$*&(# (
+ chw-__
+WS)
+S S  S S S S
+ jrrjr jr jjrjrjrjrjrjrjrrjrrjrjrj rj rjr j rj jrjr jr jrjr
+ 08  808 9 08 8 08 0 0980 8 8 809 8094023489 320492 08940924 098 092840 8932409 0 2983409 349 8489083402984
+ :>{>{:.;[[.;.;[.[;.[;.[;;.]] .;.;...;. 
+c.c.c;c.;c.;cc
+c.;cc;.c
+.;cc
+
+.';cc.'c;
+';cc
+'.;c
+';c.
+'c;.c
+';.c
+'.w;'
+.w
+
+w
+e
+     'e;.d.'
+.eef;.'f.; 
+;
+
+uBy B
+       8 98 9 9
+kkkkkKkkkkkKKKK
+J
+ JRMI J I IJ4I  I I I II  I I II II
+I
+
+I
+I
+I
+I
+
+
+I 
+I  
+
+
+I 
+I 
+I I
+
+II
+ I 
+I 
+I 
+77
+ I7I 
+I 
+I
+I
+
+II
+
+I I 
+I 
+I 
+I 
+
+
+ I
+I 
+I 
+ cpa-pop == = = = = = ==  #(@(3
+    1234
+    223
+    2
+    2
+    32
+    32
+    3
+    23
+    23
+    23
+    2
+    
